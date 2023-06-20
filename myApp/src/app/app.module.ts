2fc2355f26ec8cbbe3ad0d7ab70eb31a8bf8d5f7
samaraCassie <samaracassie@outlook.com>
2023-06-20 13:36:16 -0300

    build:
    news components

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { PagProductComponent } from './pag-product/pag-product.component';
+import { PagHomeComponent } from './pag-home/pag-home.component';
+import { PagOrderComponent } from './pag-order/pag-order.component';
+import { CardComponent } from './card/card.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagProductComponent,
+    PagHomeComponent,
+    PagOrderComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
